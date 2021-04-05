12dd0288df60b2f520af907adbf634a0ae3f6e66
Антон <omnimeta@none>
2021-04-06 00:47:22 +0100

    Various corrections and update health/ready check

--- grow-controller/src/main.py
+++ grow-controller/src/main.py
@@ -6,18 +6,25 @@ def main():
         print('Running app...')
 
         # readiness and liveliness check
-        ready_path = '/tmp/ready/check'
-        if not isfile(ready_path):
-            ready_file = None
-            try:
+        ready_path = '/tmp/health/ready'
+        live_path = '/tmp/health/alive'
+        ready_file = None
+        live_file = None
+        try:
+            if not isfile(ready_path):
                 ready_file = open(ready_path, 'w')
                 ready_file.write('Ready')
-            except Exception as err:
-                print('Error: %s' % str(err))
-            finally:
-                if ready_file is not None:
-                    ready_file.close()
-            ready_file.close()
+
+            if not isfile(live_path):
+                live_file = open(live_path, 'w')
+                live_file.write('Alive')
+        except Exception as err:
+            print('Error: %s' % str(err))
+        finally:
+            if ready_file is not None:
+                ready_file.close()
+            if live_file is not None:
+                live_file.close()
 
         sleep(2)
 
