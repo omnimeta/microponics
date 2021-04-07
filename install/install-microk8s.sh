bbf94f1e7ac172b6a99ccded3cd84fc1de988553
Антон <omnimeta@none>
2021-04-07 16:30:29 +0100

    Added boot parameter handling for arm64
    Also added some useful aliases that can be sourced

--- install/install-microk8s.sh
+++ install/install-microk8s.sh
@@ -4,8 +4,10 @@ sudo snap install microk8s --classic --channel=1.19
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
 set +x
-if [ -z "`cat /boot/firmware/cmdline.txt | grep -o 'cgroup_enable=memory'`" ]; then
-  echo 'cgroup_enable=memory cgroup_memory=1' | sudo tee -a /boot/firmware/cmdline.txt
+if [ ! -z "`uname -a | grep -o aarch64`"]; then
+  if [ -z "`cat /boot/firmware/cmdline.txt | grep -o 'cgroup_enable=memory'`" ]; then
+    echo 'cgroup_enable=memory cgroup_memory=1' | sudo tee -a /boot/firmware/cmdline.txt
+  fi
 fi
 
 echo 'Run `newgrp microk8s` to enable the necessary permisions'
