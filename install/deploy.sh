d7c211165a275520e140ff3b2d85c2b99e9cc864
Антон <omnimeta@none>
2021-04-07 14:58:41 +0100

    Streamlined install/deploy process
    Install/deploy scripts now handle both master and worker nodes

---
+++ install/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -ex
+
+IMAGE_PATH="images"
+CHART_PATH="./microponics"
+CHART_NAME="microponics"
+STORAGE_PATH="${HOME}/storage"
+DEFAULT_NODE_NAME="${HOSTNAME}"
+
+source install-microk8s
+microk8s start
+microk8s enable storage
+microk8s ctr image import ${IMAGE_PATH}/grow-controller-image.tar --no-unpack
+
+if [ "${MASTER}" = "true" ]; then
+  microk8s kubectl label nodes ${DEFAULT_NODE_NAME} storage_node=true --overwrite
+  if [ ! -d "${STORAGE_PATH}" ]; then
+    mkdir -p ${STORAGE_PATH}
+  fi
+
+  microk8s enable ingress helm3
+  microk8s ctr image import ${IMAGE_PATH}/storage-image.tar --no-unpack
+  microk8s ctr image import ${IMAGE_PATH}/frontend-image.tar --no-unpack
+  microk8s helm3 install ${CHART_NAME} ${CHART_PATH} -f ${CHART_PATH}/values.yaml --debug
+
+  unset -x
+  echo "Stabilising deployment"
+  sleep 20s
+
+else
+  microk8s kubectl label nodes ${DEFAULT_NODE_NAME} storage_node=false --overwrite
+  unset -x
+  echo 'Run `microk8s add-node` on the MASTER node - this will output a command of the form:'
+  echo '`microk8s join ip-XX-XX-XX-XX:XXXXXX/XXXXXXXXXXXXXXXXXXXXXXX` - run this command on THIS node to connect'
+fi
+
+microk8s status --wait-ready
--- install/install-microk8s.sh
+++ install/install-microk8s.sh
@@ -1,9 +1,11 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
 sudo snap install microk8s --classic --channel=1.19
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
 if [ -z "`cat /boot/firmware/cmdline.txt | grep -o 'cgroup_enable=memory'`" ]; then
+  unset -x
   echo 'cgroup_enable=memory cgroup_memory=1' | sudo tee -a /boot/firmware/cmdline.txt
+  set -x
 fi
-echo 'Run `su - $USER` to re-enter the shell session with the updates in place'
+newgrp microk8s
